f8c6fb84d7de3b75f4f5fa97ee9167e31e13cdb6
Willian VInicius Ramalho <49045509+willianramalho@users.noreply.github.com>
2023-03-02 16:20:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const tdEnviaAcesso = document.getElementById('enviaAcesso');
+const tdRecebeAcesso = document.getElementById('recebeAcesso');
+const selectObjeto = document.getElementById('selectObjeto')
+const botaoEnviaAcesso = document.getElementById('botaoEnviaAcesso');
+const botaoCancelaAcesso = document.getElementById('botaoCancelaAcesso');
+
+console.log(selectObjeto)
+
+if(botaoEnviaAcesso.addEventListener("click")
