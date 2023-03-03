c9b7928f47c77dc5092aa7d2a5cd7e476df37462
Willian VInicius Ramalho <49045509+willianramalho@users.noreply.github.com>
2023-03-03 17:17:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const tdRecebeAcesso = document.getElementById('recebeAcesso');
 const selectObjeto = document.getElementById('selectObjeto')
 const botaoEnviaAcesso = document.getElementById('botaoEnviaAcesso');
 const botaoCancelaAcesso = document.getElementById('botaoCancelaAcesso');
+const botaoSalvar = document.getElementById('botaoSalvar');
 
-console.log(selectObjeto)
 
-if(botaoEnviaAcesso.addEventListener("click")
+botaoSalvar.addEventListener("click", function () {
+    alert("lembre de preencher todos os campos");
+} )
