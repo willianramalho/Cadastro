662d23138beae4a8e0ad83d27c0cd8a3b2c5a213
Willian VInicius Ramalho <witymagts@gmail.com>
2023-03-05 18:29:26 -0300

    Uptade
    atualizações no javascript e no html, para realizar o drag and drop dos niveis de acessos

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
-const tdEnviaAcesso = document.getElementById('enviaAcesso');
-const tdRecebeAcesso = document.getElementById('recebeAcesso');
-const selectObjeto = document.getElementById('selectObjeto')
-const botaoEnviaAcesso = document.getElementById('botaoEnviaAcesso');
-const botaoCancelaAcesso = document.getElementById('botaoCancelaAcesso');
-const botaoSalvar = document.getElementById('botaoSalvar');
+const colunas = document.querySelectorAll(".colunaAcessos");
 
+// função para quando selecionar um item do tipo "draggable"
 
-botaoSalvar.addEventListener("click", function () {
-    alert("lembre de preencher todos os campos");
-} )
+document.addEventListener("dragstart", (e) =>{
+    e.target.classList.add("dragging");
+});
+
+// função para quando soltar o item dentro da Div
+
+document.addEventListener("dragend", (e) =>{
+    e.target.classList.add("dragging");
+});
+
+// forEach para cada coluna, para que em cada coluna seja adicionado o evento dragover
+
+colunas.forEach((item) => {
+    item.addEventListener("dragover", (e) => {
+        const dragging = document.querySelector(".draggin");
+        const applyAfter = getNewPosition(item, e.clientY);
+
+        if(applyAfter) {
+            applyAfter.insertAdjacentElement("afterned", dragging);
+        }else {
+            item.prepend(dragging);
+        }
+    });
+});
+
+function getNewPosition(coluna, clientY) {
+    const cards = colunas.querySelectorAll(".nivelAcessos:not(.draggin)");
+    let result;
+
+    for(let refer_card of cards) {
+        const box = refer_card.getBoundingClientRect();
+        const boxCenterY = box.y + box.height / 2;
+
+        if(posY >= boxCenterY) result = refer_card;
+    }
+    return result;
+}
