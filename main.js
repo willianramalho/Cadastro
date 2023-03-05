bb5fcb917b96eb8a0a50e7a4770f0899b256ccd8
Willian VInicius Ramalho <witymagts@gmail.com>
2023-03-05 18:43:32 -0300

    uptade da funcionalidade
    Drag and drop adicionados

--- main.js
+++ main.js
@@ -1,41 +1,38 @@
-const colunas = document.querySelectorAll(".colunaAcessos");
+// para utilizar a funcionalidade do drag and drop utilizei o video ( https://www.youtube.com/watch?v=uO-SuUhDI7k&t=41s&ab_channel=WillDev), como exemplo
 
-// função para quando selecionar um item do tipo "draggable"
+const columns = document.querySelectorAll(".column");
 
-document.addEventListener("dragstart", (e) =>{
-    e.target.classList.add("dragging");
+document.addEventListener("dragstart", (e) => {
+  e.target.classList.add("dragging");
 });
 
-// função para quando soltar o item dentro da Div
-
-document.addEventListener("dragend", (e) =>{
-    e.target.classList.add("dragging");
+document.addEventListener("dragend", (e) => {
+  e.target.classList.remove("dragging");
 });
 
-// forEach para cada coluna, para que em cada coluna seja adicionado o evento dragover
+columns.forEach((item) => {
+  item.addEventListener("dragover", (e) => {
+    const dragging = document.querySelector(".dragging");
+    const applyAfter = getNewPosition(item, e.clientY);
 
-colunas.forEach((item) => {
-    item.addEventListener("dragover", (e) => {
-        const dragging = document.querySelector(".draggin");
-        const applyAfter = getNewPosition(item, e.clientY);
-
-        if(applyAfter) {
-            applyAfter.insertAdjacentElement("afterned", dragging);
-        }else {
-            item.prepend(dragging);
-        }
-    });
-});
-
-function getNewPosition(coluna, clientY) {
-    const cards = colunas.querySelectorAll(".nivelAcessos:not(.draggin)");
-    let result;
-
-    for(let refer_card of cards) {
-        const box = refer_card.getBoundingClientRect();
-        const boxCenterY = box.y + box.height / 2;
-
-        if(posY >= boxCenterY) result = refer_card;
+    if (applyAfter) {
+      applyAfter.insertAdjacentElement("afterend", dragging);
+    } else {
+      item.prepend(dragging);
     }
-    return result;
+  });
+});
+
+function getNewPosition(column, posY) {
+  const cards = column.querySelectorAll(".item:not(.dragging)");
+  let result;
+
+  for (let refer_card of cards) {
+    const box = refer_card.getBoundingClientRect();
+    const boxCenterY = box.y + box.height / 2;
+
+    if (posY >= boxCenterY) result = refer_card;
+  }
+
+  return result;
 }
