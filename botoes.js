949cc44ce272ea9f87803e79f98bfc643bce4670
Willian VInicius Ramalho <49045509+willianramalho@users.noreply.github.com>
2023-03-06 12:06:02 -0300

    Add files via upload

---
+++ botoes.js
@@ -0,0 +1,6 @@
+const botaoCancelarAcesso = document.getElementById('.botaoCancela');
+const botaoSalvarAcesso = document.getElementById('.botaoSalvar');
+
+botaoSalvarAcesso.addEventListener("click", function(){
+    alert("Acesso criado");
+})
--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 // para utilizar a funcionalidade do drag and drop utilizei o video ( https://www.youtube.com/watch?v=uO-SuUhDI7k&t=41s&ab_channel=WillDev), como exemplo
 
-const columns = document.querySelectorAll(".column");
+const columns = document.querySelectorAll(".colunaDeAcesso");
 
-document.addEventListener("dragstart", (e) => {
-  e.target.classList.add("dragging");
+document.addEventListener("dragstart", (evento) => {
+  evento.target.classList.add("dragging");
 });
 
-document.addEventListener("dragend", (e) => {
-  e.target.classList.remove("dragging");
+document.addEventListener("dragend", (evento) => {
+  evento.target.classList.remove("dragging");
 });
 
 columns.forEach((item) => {
-  item.addEventListener("dragover", (e) => {
+  item.addEventListener("dragover", (evento) => {
     const dragging = document.querySelector(".dragging");
-    const applyAfter = getNewPosition(item, e.clientY);
+    const applyAfter = getNewPosition(item, evento.clientY);
 
     if (applyAfter) {
       applyAfter.insertAdjacentElement("afterend", dragging);
