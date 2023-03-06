b5d98f537b106eb93e5dd4e6ce8ffe8aa26524fc
Willian VInicius Ramalho <49045509+willianramalho@users.noreply.github.com>
2023-03-06 14:01:42 -0300

    Add files via upload

--- botoes.js
+++ botoes.js
@@ -1,6 +1,23 @@
-const botaoCancelarAcesso = document.getElementById('.botaoCancela');
-const botaoSalvarAcesso = document.getElementById('.botaoSalvar');
+const botaoCancelarAcesso = document.getElementById('botaoCancela');
+const botaoSalvarAcesso = document.getElementById('botaoSalvar');
+const inputVerifica = document.querySelectorAll('input');
+
+console.log(inputVerifica)
+
+
 
 botaoSalvarAcesso.addEventListener("click", function(){
+    for (let i = 0; i < inputVerifica.length; i++) {
+        if (inputVerifica[i].value === "") {
+            alert("Por favor, preencha todos os campos antes de prosseguir.");
+            return;
+        }
+    }
     alert("Acesso criado");
-})
+});
+
+botaoCancelarAcesso.addEventListener("click", function (){
+    window.location.reload();
+    alert("Processo de acesso cancelado")
+});
+
